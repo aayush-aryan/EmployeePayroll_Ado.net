89ff178cf37a6aae784572797c6489729e617be1
aayush-aryan <log2aayusharyan@gmail.com>
2022-09-05 17:23:23 +0530

    creating database connections

---
+++ EmployeePayroll/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayroll
+{
+    public class EmployeeRepository
+    {
+        public static string connectionString = @"Data Source=DESKTOP-EPUK1PJ;Initial Catalog=payrolls_service;
+                  Integrated Security=True;";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
